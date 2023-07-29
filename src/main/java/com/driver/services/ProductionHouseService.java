d0c3718a735e60f63bb1b34fffaaca3b892ac51b
Darpan Ramani <ramanidarpan3@gmail.com>
2023-07-29 17:02:02 +0530

    hotstar

--- src/main/java/com/driver/services/ProductionHouseService.java
+++ src/main/java/com/driver/services/ProductionHouseService.java
@@ -3,9 +3,14 @@ package com.driver.services;
 
 import com.driver.EntryDto.ProductionHouseEntryDto;
 import com.driver.model.ProductionHouse;
+import com.driver.model.WebSeries;
 import com.driver.repository.ProductionHouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductionHouseService {
@@ -15,7 +20,11 @@ public class ProductionHouseService {
 
     public Integer addProductionHouseToDb(ProductionHouseEntryDto productionHouseEntryDto){
 
-        return  null;
+        ProductionHouse productionHouse = new ProductionHouse();
+        productionHouse.setName(productionHouseEntryDto.getName());
+        productionHouse.setRatings(0);
+
+        return productionHouseRepository.save(productionHouse).getId();
     }
 
 
